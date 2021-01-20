0353fdc3a0094ae34e8ae853afb28594ae994314
ono_Kyosuke <k020g1214@g.neec.ac.jp>
2021-01-20 14:28:37 +0900

    Revert "ローカルでコンフリクト変更"
    This reverts commit 6477c85ad6c4a536f4fc3fbb40f2ed4994318cf8.

--- TestGit/TestGit.cpp
+++ TestGit/TestGit.cpp
@@ -7,7 +7,6 @@ int main()
 {
     std :: cout  <<  "Hello T World！HAHAHA \ n" ;
      std :: cout  <<  "Hello The World！HAHAHA \ n" ;
-     std::cout << "ローカルリポジトリで変更した場所\ n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
